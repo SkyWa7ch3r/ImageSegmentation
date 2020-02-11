e5ec46724d7c17c07ebce8db0628c883568f9a0f
SkyWa7ch3r <jd427244@gmail.com>
2020-02-11 23:10:10 +0800

    Full Resolution Training Possible now

--- separable_unet.py
+++ separable_unet.py
@@ -31,11 +31,11 @@ def model(num_classes=20, input_size=(1024, 2048, 3)):
     ### Beginning of encoder ###
     inputs = keras.Input(input_size)
     # Encoder/Scaling Down
-    conv1 = conv_layer(inputs, 32, 3)
-    conv2 = conv_layer(conv1, 32, 3, separable=True)
+    conv1 = conv_layer(inputs, 16, 3)
+    conv2 = conv_layer(conv1, 16, 3, separable=True)
     pool1 = MaxPooling2D(pool_size=(2, 2))(conv2)
-    conv3 = conv_layer(pool1, 64, 3)
-    conv4 = conv_layer(conv3, 64, 3, separable=True)
+    conv3 = conv_layer(pool1, 32, 3)
+    conv4 = conv_layer(conv3, 32, 3, separable=True)
     pool2 = MaxPooling2D(pool_size=(2, 2))(conv4)
     conv5 = conv_layer(pool2, 128, 3)
     conv6 = conv_layer(conv5, 128, 3, separable=True)
@@ -61,15 +61,16 @@ def model(num_classes=20, input_size=(1024, 2048, 3)):
     conv13 = conv_layer(merge2, 128, 3, separable=True)
     conv14 = conv_layer(conv13, 128, 3, separable=True)
 
-    up3 = conv_layer(UpSampling2D(size=(2, 2))(conv14), 64, 2, separable=True)
+    up3 = conv_layer(UpSampling2D(size=(2, 2))(conv14),
+                     32, 2, separable=True, padding='same')
     merge3 = concatenate([conv4, up3], axis=3)
-    conv15 = conv_layer(merge3, 64, 3, separable=True)
-    conv16 = conv_layer(conv15, 64, 3, separable=True)
+    conv15 = conv_layer(merge3, 32, 3, separable=True)
+    conv16 = conv_layer(conv15, 32, 3, separable=True)
 
-    up4 = conv_layer(UpSampling2D(size=(2, 2))(conv16), 32, 2, separable=True)
+    up4 = conv_layer(UpSampling2D(size=(2, 2))(conv16), 16, 2, separable=True)
     merge4 = concatenate([conv2, up4], axis=3)
-    conv17 = conv_layer(merge4, 32, 3, separable=True)
-    conv18 = conv_layer(conv17, 32, 3, separable=True)
+    conv17 = conv_layer(merge4, 16, 3, separable=True)
+    conv18 = conv_layer(conv17, 16, 3, separable=True)
     conv19 = conv_layer(conv18, 2, 3)
     conv20 = Conv2D(num_classes, kernel_size=(1, 1), strides=(
         1, 1), activation='softmax', dtype=tf.float32)(conv19)
