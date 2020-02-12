1ed89308c6d4436da6c7697f76d4f48bf46a360b
SkyWa7ch3r <jd427244@gmail.com>
2020-02-12 11:48:49 +0800

    Changed Separable Unet to SGD Nesterov with Momentum 0.9

--- train.py
+++ train.py
@@ -240,9 +240,9 @@ if model_name == 'separable_unet':
     if args.learning_rate:
         learning_rate = args.learning_rate*hvd.size()
     else:
-        learning_rate = 0.001*hvd.size()
+        learning_rate = 0.003*hvd.size()
     model = separable_unet.model(input_size=target_size, num_classes=CLASSES)
-    optimizer = keras.optimizers.Adam(learning_rate=learning_rate)
+    optimizer = keras.optimizers.SGD(learning_rate=learning_rate, momentum=0.9, nesterov=True)
 elif model_name == 'bayes_segnet':
     if args.learning_rate:
         learning_rate = args.learning_rate*hvd.size()
